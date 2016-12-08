3cac183e6d2bc320f72c583ce36408e5f331582a
XunShan <huangdong.li@outlook.com>
2016-12-08 23:16:27 +0800

    add ssl reading link

---
+++ src/main/java/xunshan/net/protocol/ssl/OkSslImpl.java
@@ -0,0 +1,13 @@
+package xunshan.net.protocol.ssl;
+
+/**
+ * OkHttp implement for ssl
+ * 1. learn how to build ssl job in client
+ * 2. extract okhttp impl to an independent module
+ *
+ * link
+ * 1.http://blog.csdn.net/lmj623565791/article/details/48129405
+ * 2.http://security.stackexchange.com/questions/20803/how-does-ssl-tls-work
+ */
+public class OkSslImpl {
+}
