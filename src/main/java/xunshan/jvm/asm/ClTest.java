f3e66b630f862d9a22d40f179ee73977c0dd22a9
XunShan <huangdong.li@outlook.com>
2017-01-14 13:36:05 +0800

    ASM framework demo

---
+++ src/main/java/xunshan/jvm/asm/ClTest.java
@@ -0,0 +1,10 @@
+package xunshan.jvm.asm;
+
+/**
+ * Created by xunshan on 17-1-7.
+ */
+public class ClTest {
+    public static void main(String[] args) {
+        System.out.println("begin...");
+    }
+}
